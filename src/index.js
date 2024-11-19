6a37cd3af17716dcb80482edb9e935927f4bda49
Deepak Prasanna Ramaswami <deepak@Deepaks-MacBook-Pro.local>
2024-11-19 10:25:51 -0400

    React compiler.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 import React, { useState } from 'react';
 import { createRoot } from 'react-dom/client';
 
-const App = () => {
-    const [count, setCount] = useState(0);
-    return (
-        <div>
-            <h1>Count: {count}</h1>
-            <button onClick={() => setCount(count + 1)}>Increment</button>
-        </div>
-    );
-};
+function Header() {
+  console.log("Header", Math.random());
+  return (
+    <header>
+      <h1>React Counter</h1>
+    </header>
+  );
+}
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Header />
+      <div>
+        <p>{count}</p>
+        <button onClick={() => setCount(count + 1)}>Increment</button>
+      </div>
+    </>
+  );
+}
 
 const root = createRoot(document.getElementById('root'));
 root.render(<App />);
